fee96456da0f79d8c255fab04f1c42881a3c5d3a
Deepranjan-Bharali <149260692+Deepranjan-Bharali@users.noreply.github.com>
2024-08-23 22:47:50 +0530

    Add files via upload

---
+++ views/p2.js
@@ -0,0 +1,9 @@
+   document.getElementById("memory").addEventListener("click", function() {
+        window.location.href = "memory.html";
+    });
+    document.getElementById("wish").addEventListener("click", function() {
+        window.location.href = "wishes.html";
+    });
+    document.getElementById("gift").addEventListener("click", function() {
+        window.location.href = "gift.html";
+    });
